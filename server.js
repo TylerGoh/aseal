292e0d853f037b218ab1520e3f9a1f1cb6e25e40
TylerGoh <tylergoh51@gmail.com>
2018-10-08 19:54:21 +0800

    small change

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(process.env.PORT || 1337);
+serv.listen(process.env.PORT || 1337 || 80);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
